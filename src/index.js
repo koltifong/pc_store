140e516cce0a8797fa1c013bb8ee8b2fd553d405
unknown <em.vannin.2001@gmail.com>
2022-02-05 08:57:59 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { userContext } from './pages/products/context';
 
+const port = process.env.PORT || 3000;
+App.listen(port, () => console.log('listening song on port 3000'))
+
 ReactDOM.render(
-  
+  <React.StrictMode>
     <userContext>
       <App />
     </userContext>
+  </React.StrictMode>
   ,
   document.getElementById('root')
 );
