71f275d78bd8e56a9ada59a9eab44043004da763
unknown <em.vannin.2001@gmail.com>
2022-02-06 13:57:39 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { useContext } from 'react';
 
+var http = require("http");
+setInterval(function() {
+    http.get("http://<pcpart-picker>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
